49085ffeb2d77472c7bfaaf8d363c1aa56381e13
Sandeepchalla287 <61781407+Sandeepchalla287@users.noreply.github.com>
2021-03-03 20:19:31 +0530

    2d answer

--- answer2d.js
+++ answer2d.js
@@ -1,3 +1,4 @@
+const arrOne = [1, 4, 6, 2, 1];
 function checkUniqueness(arr) {
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length; j++) {
@@ -12,3 +13,5 @@ function checkUniqueness(arr) {
         
     };
 };
+
+console.log(checkUniqueness(arrOne));
