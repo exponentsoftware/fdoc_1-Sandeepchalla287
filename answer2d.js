c7d428b300f231941860399b898eaafe4724e1fb
Sandeepchalla287 <61781407+Sandeepchalla287@users.noreply.github.com>
2021-03-03 20:18:10 +0530

    2d answer

---
+++ answer2d.js
@@ -0,0 +1,14 @@
+function checkUniqueness(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if(arr[i] !== arr[j]) {
+                return true;
+            }else {
+                return false;
+            };
+
+        };
+        
+        
+    };
+};
