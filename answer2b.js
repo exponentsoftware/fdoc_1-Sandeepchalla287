2d7e2bb2414ab4decd893c2f206cb978a98ce39a
Sandeepchalla287 <61781407+Sandeepchalla287@users.noreply.github.com>
2021-03-03 17:09:01 +0530

    2b answer

---
+++ answer2b.js
@@ -0,0 +1,8 @@
+let arr = [];
+for(let i =1; i <= 7; i++){
+    var num = Math.floor(Math.random() * 9) + 1;
+    if(arr.indexOf(num) === -1) {
+        arr.push(num);
+    };
+};
+console.log(arr);
