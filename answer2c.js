a5a1fa9ca29a198128c5afa8f63213a89bb2c8b8
Sandeepchalla287 <61781407+Sandeepchalla287@users.noreply.github.com>
2021-03-03 17:16:26 +0530

    3b answer

---
+++ answer2c.js
@@ -0,0 +1,10 @@
+function reverseArray(array) {
+    var arr = [];
+    while (array.length) {
+      arr.push(array.pop());
+    };
+  
+    return arr;
+  };
+  
+  console.log(reverseArray(["A", "B", "C"]));
